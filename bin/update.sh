36f81273dba214983495a22e7a1d7109bf71fce5
Ratan Varghese <19657319+ratanvarghese@users.noreply.github.com>
2018-08-19 00:01:56 -0400

    Construct JSON Feed

--- bin/update.sh
+++ bin/update.sh
@@ -3,4 +3,11 @@ for dir in ./*; do
 	sh bin/article.sh -A "$dir" -T ./template
 done
 
-cat */item.json | jq -s 'sort_by(.date_published) | reverse'
+items=$(cat */item.json \
+		| jq -s 'sort_by(.date_published) | reverse | del(.[]._ratan_blog_class)')
+
+jq -n -c -f template/jfroot.jq.json \
+	--argjson ITEMS "$items" \
+	| tee feeds/json \
+	| gzip --keep --best > feeds/json.gz
+
